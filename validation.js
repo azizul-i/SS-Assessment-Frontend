bb511ce6b538882e7d91b4e8463eb5b3a5380288
Azizul <misl000@aucklanduni.ac.nz>
2020-09-28 12:51:01 +1300

    Added specific validation comments, new summary page

--- validation.js
+++ validation.js
@@ -1,12 +1,30 @@
 $(function () {
-  var isNewMailingAddRequired = true;
   $("input").on("blur", function () {
     if ($("#AddEmployeeForm").valid()) {
       $("#submit").prop("disabled", false);
     } else {
       $("#submit").prop("disabled", true);
     }
+
+    if ($("#AsAbove").is(":checked")) {
+      $("#NewMailingAddress").prop("disabled", true);
+    } else {
+      $("#NewMailingAddress").prop("disabled", false);
+    }
   });
+
+  $.validator.addMethod(
+    "greaterThanLastYear",
+    function (value, element) {
+      var currentDate = new Date();
+      currentDate.setFullYear(currentDate.getFullYear() - 1);
+      if (new Date(value)) {
+        return new Date(value) > new Date(currentDate);
+      }
+      return false;
+    },
+    "Employee start date must be within the last year or in the future."
+  );
 
   if (!$("#FirstName").val()) {
     $("#submit").prop("disabled", true);
@@ -55,6 +73,52 @@ $(function () {
         required: () => {
           return $("#AsAbove").is(":not(:checked)");
         },
+      },
+      StartDate: {
+        required: true,
+        greaterThanLastYear: true,
+      },
+    },
+    messages: {
+      FirstName: {
+        required: "Please enter employee's first name",
+      },
+      EmailAddress: {
+        required: "Please enter employee's email address",
+        email: "Please enter a valid email address",
+      },
+      LastName: {
+        required: "Please enter employee's last name",
+      },
+      FullAddress: {
+        required: "Please enter employee's current address",
+      },
+      PhoneNumber: {
+        required: "Please enter employee's phone number",
+      },
+      CitizenStatus: {
+        required: "Please enter employee's citizenship status",
+      },
+      StartDate: {
+        required: "Please enter employee's starting date (within last year)",
+      },
+      EmploymentType: {
+        required: "Please enter the type of employment",
+      },
+      PositionTitle: {
+        required: "Please enter employment position",
+      },
+      EmergencyName: {
+        required: "Please enter emergency contact name",
+      },
+      EmergencyRelationship: {
+        required: "Please enter emergency contact relationship",
+      },
+      EmergencyNumber: {
+        required: "Please enter emergency contact number",
+      },
+      NewMailingAddress: {
+        required: "Enter a new mailing address or check same 'As Above'",
       },
     },
   });
