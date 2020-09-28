22c9022288eb6d74bc46d1dc34a9eac902ea1c5b
Azizul <misl000@aucklanduni.ac.nz>
2020-09-28 14:28:21 +1300

    Added get Form Data from Server

---
+++ api.js
@@ -0,0 +1,21 @@
+const getEmployeeData = async () => {
+  const response = await fetch(
+    "https://addemployeess.azurewebsites.net/api/Employees/" +
+      localStorage.getItem("Id"),
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((result) => {
+      const finalRes = result;
+      console.log("Response: ", result);
+    });
+
+  return response;
+};
